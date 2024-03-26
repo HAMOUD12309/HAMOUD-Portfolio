e3fbcc8cb0e06303ad6fb736cf73271d73b6580a
HAMOUD12309 <162879414+HAMOUD12309@users.noreply.github.com>
2024-03-26 09:34:45 +0000

    .

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import load_projects_from_db, load_project_from_db
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-  return render_template("home.html")
+def hello_world():
+  projects = load_projects_from_db()
+  return render_template("home.html", projects=projects)
+
+
+@app.route("/project/<id>")
+def project_details(id):
+  project = load_project_from_db(id)
+  return render_template("project_details.html", project=project)
+
+@app.route("/message")
+def message():
+  return render_template("contactus.html")
+
+@app.route("/message_submitted", methods=["POST"])
+def message_submitted():
+  mdata = request.form
+  return render_template("message_submitted.html", data=mdata)
+
 
 
 if __name__ == "__main__":
